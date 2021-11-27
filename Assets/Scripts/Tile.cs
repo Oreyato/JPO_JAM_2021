a094e260535c53e91b1c06bc9a5486e46fee110f
GuillaumeRoeder <55831311+GuillaumeRoeder@users.noreply.github.com>
2021-11-27 14:44:55 +0100

    Tile de base créée

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public GameObject logGeo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instantiate(logGeo, new Vector3(-4, 0, 1), Quaternion.identity);
+        //Instantiate(logGeo, new Vector3(4, 0, 1), Quaternion.identity);
+        //Instantiate(logGeo, new Vector3(0, 0, -2), Quaternion.identity);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
