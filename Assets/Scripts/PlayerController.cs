39ef5d158748fc333fc5e59f8eed6c2bf17e7f79
Aenorya <rjacobson@artfx.fr>
2021-11-27 13:45:03 +0100

    controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -29,7 +29,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gamepad!= null && gamepad.buttonSouth.isPressed)
+        if (gamepad!= null && gamepad.buttonSouth.wasPressedThisFrame)
         {
             OnActionInput();
         }
